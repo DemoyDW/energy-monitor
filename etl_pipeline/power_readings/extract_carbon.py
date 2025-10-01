780be7320059949c092c4cc97c0d8d955d9b3ee7
niall-dance-sanchez <niall0205@hotmail.co.uk>
2025-10-01 11:05:59 +0100

    extract scipt for carbon intensity completed

---
+++ etl_pipeline/power_readings/extract_carbon.py
@@ -0,0 +1,23 @@
+"""Script with the functions to extract the carbon intensity data from the NESO API."""
+
+from datetime import datetime, timedelta, timezone
+from requests import get
+
+
+def get_utc_settlement_time() -> list[str]:
+    """Get the settlement time in UTC."""
+
+    end_time = datetime.now(timezone.utc)
+    start_time = (end_time - timedelta(minutes=29))
+
+    return [start_time.isoformat(), end_time.isoformat()]
+
+
+def extract_carbon_intensity_data(start_time: str, end_time: str) -> list[dict]:
+    """Extracts the carbon intensity data from the API for a given time window."""
+
+    url = f"https://api.carbonintensity.org.uk/generation/{start_time}/{end_time}"
+    response = get(url)
+    data = response.json()["data"][0]["generationmix"]
+
+    return data
