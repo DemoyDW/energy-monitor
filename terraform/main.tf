f76db1b15f4c5ffd200e97791773b52198e71bb5
DemoyDW <demoywitter@gmail.com>
2025-10-01 20:15:39 +0100

    Making the rds publicly accessible

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,6 +44,7 @@ resource "aws_db_instance" "c19-energy-monitor-rds" {
   skip_final_snapshot  = true
   vpc_security_group_ids = [aws_security_group.c19-rds-sg.id]
   db_subnet_group_name = aws_db_subnet_group.c19-subnet-groups.name
+  publicly_accessible = true
 }
 
 
