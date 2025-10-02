d5bf9005c07ac773378571cc616988ae435e1cda
Midul <miduluddin03@gmail.com>
2025-10-02 10:42:30 +0100

    changed functions in to constant variables for consistency

--- etl_pipeline/power_readings/transform.py
+++ etl_pipeline/power_readings/transform.py
@@ -6,6 +6,34 @@ import pandas as pd
 from extract import get_utc_settlement_time, get_demand_summary, get_energy_pricing, get_generation_by_type, get_national_energy_generation
 
 
+INTERCONNECTOR_MAP = {
+    "INTELEC": "Belgium (ElecLink)",
+    "INTEW": "Ireland (East-West)",
+    "INTFR": "France (IFA)",
+    "INTIFA2": "France (IFA2)",
+    "INTIRL": "Northern Ireland (Moyle)",
+    "INTNED": "Netherlands (BritNed)",
+    "INTNEM": "Belgium (Nemo Link)",
+    "INTNSL": "Norway (North Sea Link)",
+    "INTVKL": "Denmark (Viking Link)",
+    "INTGRNL": "Ireland (Greenlink)"
+
+}
+
+COUNTRY_MAP = {
+    'Belgium (ElecLink)': 'Belgium',
+    'Belgium (Nemo Link)': 'Belgium',
+    'France (IFA)': 'France',
+    'France (IFA2)': 'France',
+    'Ireland (East-West)': 'Ireland',
+    'Ireland (Greenlink)': 'Ireland',
+    'Denmark (Viking Link)': 'Denmark',
+    'Netherlands (BritNed)': 'Netherlands',
+    'Northern Ireland (Moyle)': 'Northern Ireland',
+    'Norway (North Sea Link)': 'Norway',
+}
+
+
 def calculate_avg_for_last_settlement(df: pd.DataFrame, column: str) -> float:
     """Calculate average of a numeric column of a dataframe for the last settlement."""
 
@@ -20,23 +48,6 @@ def calculate_avg_for_last_settlement(df: pd.DataFrame, column: str) -> float:
     settlement = df[df['startTime'].between(start, end)]
     avg = settlement[column].mean()
     return avg
-
-
-def country_mappings() -> dict:
-    """Map acronym to correct country for energy generation"""
-    interconnector_map = {
-        "INTELEC": "Belgium (ElecLink)",
-        "INTEW": "Ireland (East-West)",
-        "INTFR": "France (IFA)",
-        "INTIFA2": "France (IFA2)",
-        "INTIRL": "Northern Ireland (Moyle)",
-        "INTNED": "Netherlands (BritNed)",
-        "INTNEM": "Belgium (Nemo Link)",
-        "INTNSL": "Norway (North Sea Link)",
-        "INTVKL": "Denmark (Viking Link)",
-        "INTGRNL": "Ireland (Greenlink)"
-    }
-    return interconnector_map
 
 
 def summarize_energy_generation(df: pd.DataFrame, mappings: dict) -> dict:
@@ -59,22 +70,12 @@ def summarize_energy_generation(df: pd.DataFrame, mappings: dict) -> dict:
 def combine_company_generation(df: pd.DataFrame) -> pd.DataFrame:
     """Combine different parts of a country to 1 country"""
     # map to group all relevant countries in to one
-    country_map = {
-        'Belgium (ElecLink)': 'Belgium',
-        'Belgium (Nemo Link)': 'Belgium',
-        'France (IFA)': 'France',
-        'France (IFA2)': 'France',
-        'Ireland (East-West)': 'Ireland',
-        'Ireland (Greenlink)': 'Ireland',
-        'Denmark (Viking Link)': 'Denmark',
-        'Netherlands (BritNed)': 'Netherlands',
-        'Northern Ireland (Moyle)': 'Northern Ireland',
-        'Norway (North Sea Link)': 'Norway',
-    }
 
-    df['country'] = df['country'].map(country_map)
+    df['country'] = df['country'].map(COUNTRY_MAP)
 
     result = df.groupby('country', as_index=False)['generation'].sum()
+
+    result['generation'] = result['generation'].round(2)
 
     return result
 
@@ -104,8 +105,7 @@ def transform_all_data(time: list) -> list:
     # Energy generation by country
     imports = get_generation_by_type(time[0].replace(
         '+00:00', 'Z'), time[1].replace('+00:00', 'Z'))
-    mapped_countries = country_mappings()
-    summary = summarize_energy_generation(imports, mapped_countries)
+    summary = summarize_energy_generation(imports, INTERCONNECTOR_MAP)
     combined_countries = combine_company_generation(summary)
     all_data.extend(combined_countries['generation'].tolist())
 
