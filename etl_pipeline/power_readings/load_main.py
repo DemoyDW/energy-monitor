0c3454c2add1fcdea415754d3ddeb5dde41bccd0
niall-dance-sanchez <niall0205@hotmail.co.uk>
2025-10-02 19:15:17 +0100

    handler function complete, however might edit the transform power to be split into extract and transform instead of doing it in one

---
+++ etl_pipeline/power_readings/load_main.py
@@ -0,0 +1,24 @@
+"""Script that compiles both ETL processes to upload the power and carbon intensity data to the database."""
+
+from extract_carbon import extract_carbon_intensity_data
+from transform_carbon import transform_carbon_intensity_data
+from load_carbon import load_carbon_intensity_data, get_db_connection
+
+from extract_power import get_utc_settlement_time
+from transform_power import transform_power_data
+from load_power import load_power_reading_data
+
+
+def handler(event=None, context=None) -> dict:
+    """Load the data from the carbon and power ETL processes to the database."""
+    conn = get_db_connection()
+
+    start_time, end_time = get_utc_settlement_time()
+    carbon_data = extract_carbon_intensity_data(start_time, end_time)
+    carbon_data = transform_carbon_intensity_data(carbon_data)
+    load_carbon_intensity_data(conn, carbon_data)
+
+    power_data = transform_power_data(start_time, end_time)
+    load_power_reading_data(conn, power_data)
+
+    return {"statusCode": 200}
