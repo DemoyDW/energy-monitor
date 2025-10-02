8c267b0d4e2dac069587adb50a8f46b7be085808
DemoyDW <demoywitter@gmail.com>
2025-10-02 11:09:03 +0100

    Setting up the ECRs through terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,3 +48,48 @@ resource "aws_db_instance" "c19-energy-monitor-rds" {
 }
 
 
+resource "aws_ecr_repository" "c19-energy-monitor-readings" {
+  name = "c19-energy-monitor-readings"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "c19-energy-monitor-outages" {
+  name = "c19-energy-monitor-outages"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "c19-energy-monitor-summary" {
+  name = "c19-energy-monitor-summary"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "c19-energy-monitor-alert" {
+  name = "c19-energy-monitor-alert"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "c19-energy-monitor-dashboard" {
+  name = "c19-energy-monitor-dashboard"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
