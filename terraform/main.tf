899343fb5a7ed60eaa82c6ca078ac7abd8fd2c28
catalina <trainee.catalina.creanga@sigmalabs.co.uk>
2025-10-03 11:38:54 +0100

    Terraform lambda

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,49 @@ provider "aws" {
   secret_key = var.SECRET_KEY
 }
 
+# # IAM role for Lambda execution
+# data "aws_iam_policy_document" "assume_role" {
+#   statement {
+#     effect = "Allow"
+
+#     principals {
+#       type        = "Service"
+#       identifiers = ["lambda.amazonaws.com"]
+#     }
+
+#     actions = ["sts:AssumeRole"]
+#   }
+# }
+
+# resource "aws_iam_role" "c19-etl-lambda-role" {
+#   name               = "c19-energy-etl-lambda-role"
+#   assume_role_policy = data.aws_iam_policy_document.assume_role.json
+# }
+
+# # Lambda Function 
+# resource "aws_lambda_function" "c19-energy-monitor-etl-lambda" {
+#   function_name = "c19-energy-et-lambda"
+#   role          = aws_iam_role.c19-etl-lambda-role.arn
+#   package_type  = "Image"
+#   image_uri     = "${aws_ecr_repository.example.repository_url}:latest"
+
+#   environment {
+#     variables = {
+#       ACCESS_KEY = var.ACCESS_KEY,
+#       SECRET_ACCESS_KEY = var.ACCESS_KEY,
+#       REGION = var.REGION,
+#       DB_PORT = var.DB_PORT,
+#       DB_NAME = var.DB_NAME,
+#       DB_USERNAME = var.DB_USERNAME,
+#       DB_PASSWORD = var.DB_PASSWORD
+#     }
+#   }
+
+#   memory_size = 512
+#   timeout     = 30
+
+#   architectures = ["arm64"] # Graviton support for better price/performance
+# }
 
 resource "aws_security_group" "c19-rds-sg" {
     name = "c19-energy-monitor-rds-sg"
@@ -46,6 +89,8 @@ resource "aws_db_instance" "c19-energy-monitor-rds" {
   db_subnet_group_name = aws_db_subnet_group.c19-subnet-groups.name
   publicly_accessible = true
 }
+
+# DASHBOARD ECS TASK DEFINITION
 
 
 resource "aws_ecr_repository" "c19-energy-monitor-readings" {
