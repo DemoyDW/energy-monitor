14dd190d67d354f4dca3c8a9cbd000dad32eb66f
niall-dance-sanchez <niall0205@hotmail.co.uk>
2025-10-03 12:31:59 +0100

    fixed leaving connection open in handler

--- etl_pipeline/power_readings/load_main.py
+++ etl_pipeline/power_readings/load_main.py
@@ -11,15 +11,17 @@ from load_power import load_power_reading_data
 
 def handler(event=None, context=None) -> dict:
     """Load the data from the carbon and power ETL processes to the database."""
-    conn = get_db_connection()
 
     start_time, end_time = get_utc_settlement_time()
+
     carbon_data = extract_carbon_intensity_data(start_time, end_time)
     carbon_data = transform_carbon_intensity_data(carbon_data)
-    load_carbon_intensity_data(conn, carbon_data)
 
     # Transform gets the data inside of the function
     power_data = transform_power_data(start_time, end_time)
-    load_power_reading_data(conn, power_data)
+
+    with get_db_connection() as conn:
+        load_carbon_intensity_data(conn, carbon_data)
+        load_power_reading_data(conn, power_data)
 
     return {"statusCode": 200}
