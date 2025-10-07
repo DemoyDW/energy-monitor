6974fce0bef6c8cb9612d1999f61749715fe5282
niall-dance-sanchez <niall0205@hotmail.co.uk>
2025-10-07 16:27:27 +0100

    first bit of parameterised testing

--- etl_pipeline/power_readings/test_transform_power.py
+++ etl_pipeline/power_readings/test_transform_power.py
@@ -4,21 +4,28 @@ Tests for transform script of power readings
 from datetime import datetime, timedelta, timezone
 import pandas as pd
 from transform_power import calculate_avg_demand_last_settlement, summarise_energy_generation
+import pytest
+
+SETTLEMENT = datetime.now(timezone.utc) - timedelta(minutes=5)
 
 
-def test_calc_avg_demand_last_settlement():
-    now = datetime.now(timezone.utc) - timedelta(minutes=5)
+@pytest.mark.parametrize(("times", "values", "average"), [
+    ([SETTLEMENT - timedelta(minutes=5 + i * 5) for i in range(6)],
+     [400, 500, 600, 700, 550, 600], 558.33),
+    ([SETTLEMENT], [0], 0),
+    ([SETTLEMENT - timedelta(minutes=25 + i * 5) for i in range(4)],
+     [100, 200, 300, 400], 200)
+])
+def test_calc_avg_demand_last_settlement(times, values, average):
 
-    times = [now - timedelta(minutes=i * 5) for i in range(6)]
-    values = [400, 500, 600, 700, 550, 600]
     df = pd.DataFrame({
         'startTime': times,
         'demand': values
     })
 
-    avg = calculate_avg_demand_last_settlement(df)
+    test_avg = calculate_avg_demand_last_settlement(df)
 
-    assert round(avg, 2) == 558.33
+    assert round(test_avg, 2) == average
 
 
 def test_summarise_energy():
