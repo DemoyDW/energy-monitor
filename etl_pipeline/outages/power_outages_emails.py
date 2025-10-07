3c07e46c851ea73e5303385faa72453925512c13
catalina <trainee.catalina.creanga@sigmalabs.co.uk>
2025-10-07 17:03:32 +0100

    HTML table conversion

--- etl_pipeline/outages/power_outages_emails.py
+++ etl_pipeline/outages/power_outages_emails.py
@@ -5,7 +5,7 @@ from load_outages import get_db_connection
 
 def sql_query_new_outages() -> str:
     """Returns a SQL string to query the database and retrieve postcode and emails affected by current power outages."""
-    return """SELECT opl.outage_id, start_time, etr, c2.category, postcode
+    return """SELECT opl.outage_id, start_time, etr, c2.category, postcode, c.customer_id, c.customer_name, c.customer_email
     FROM outage 
     JOIN outage_postcode_link opl 
     USING(outage_id)
@@ -13,6 +13,10 @@ def sql_query_new_outages() -> str:
     USING(category_id)
     JOIN postcode p 
     USING(postcode_id)
+    JOIN customer_postcode_link cpl 
+    USING(postcode_id)
+    JOIN customer c 
+    USING(customer_id)
     WHERE outage.status 
     LIKE 'current';"""
 
@@ -27,7 +31,9 @@ def get_outages_data() -> dict:
 def email_body():
     """Generate an html email with relevant postcodes for outages alerts."""
     data = get_outages_data()
-
+    email_data = data[['outage_id', 'postcode',
+                       'start_time', 'etr', 'category']]
+    table = email_data.to_html()
     email_template = f"""
     <html>
     <head>
@@ -35,25 +41,13 @@ def email_body():
     </head>
     <body>
         <h1>⚠️ Power Outage Alert</h1>
-        <h2>We are currently experiencing power outages in your area</h2>
-        <table style="width:100%">
-            <tr>
-                <th>{data.columns}</th>
-            </tr>
-            <tr>
-                <td>{data['postcode']}</td>
-            </tr>
-        </table>
+        <h2>We are currently experiencing power outages in your area!</h2>
+        {table}
     </body>
     </html>
     """
+
     print(email_template)
-    # return {
-    #     "statusCode": 200,
-    #     "body": {
-    #         "html_report": email_template
-    #     }
-    # }
 
 
 if __name__ == "__main__":
