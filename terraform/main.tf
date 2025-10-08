9a9330890ecd062a8b6448569dfd3f36e93ea71a
niall-dance-sanchez <niall0205@hotmail.co.uk>
2025-10-08 16:06:52 +0100

    removed redundant ecr

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,6 +74,33 @@ resource "aws_lambda_function" "c19-energy-outage-etl-lambda" {
 
 }
 
+
+
+
+resource "aws_lambda_function" "c19-energy-summary-email-lambda" {
+  function_name = "c19-energy-generation-etl-lambda"
+  role          = aws_iam_role.c19-etl-lambda-role.arn
+  package_type  = "Image"
+  image_uri     = "${aws_ecr_repository.c19-energy-monitor-readings.repository_url}:latest"
+
+  environment {
+    variables = {
+      ACCESS_KEY = var.ACCESS_KEY,
+      SECRET_ACCESS_KEY = var.ACCESS_KEY,
+      REGION = var.REGION,
+      DB_HOST = var.DB_HOST,
+      DB_PORT = var.DB_PORT,
+      DB_NAME = var.DB_NAME,
+      DB_USERNAME = var.DB_USERNAME,
+      DB_PASSWORD = var.DB_PASSWORD
+    }
+  }
+
+  memory_size = 512
+  timeout     = 30
+
+}
+
 resource "aws_db_instance" "c19-energy-monitor-rds" {
   allocated_storage    = 10
   identifier           = "c19-energy-monitor-rds"
@@ -217,15 +244,6 @@ resource "aws_ecr_repository" "c19-energy-monitor-outages" {
 
 resource "aws_ecr_repository" "c19-energy-monitor-summary" {
   name = "c19-energy-monitor-summary"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
-resource "aws_ecr_repository" "c19-energy-monitor-alert" {
-  name = "c19-energy-monitor-alert"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
